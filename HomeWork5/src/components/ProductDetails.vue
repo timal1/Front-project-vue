b3c8d6e2caf1d872df64118f5dc1e92d51c31ba8
tladatko.adm <tladatko@sitronics.com>
2024-03-16 09:24:59 +0300

    add HW5

---
+++ HomeWork5/src/components/ProductDetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <form>
+        <h1>Product: {{ product.name }}</h1>
+        <p>Price: {{ formattedPrice }}</p>
+        <p v-if="product.available">Available</p>
+        <p v-else>Out of stock</p>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            product:
+            {
+                name: "Samsung Galaxy",
+                price: 800,
+                available: true,
+            },
+        };
+    },
+
+    computed: {
+        formattedPrice: function () {
+            return `$${this.product.price}`;
+        }
+    }
+}
+
+
+</script>
+
+<style lang="scss" scoped></style>
